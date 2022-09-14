d9fac59fa8440571f47b912acfc0c3cac83ef98b
Jcupzz <jcupzz255@gmail.com>
2022-09-14 20:35:03 +0530

    DVR changed

--- NL/DVR.c
+++ NL/DVR.c
@@ -1,42 +1,58 @@
 #include <stdio.h>
-struct node
-{
-unsigned dist[20];
-unsigned from[20];
-}rt[10];
 int main()
 {
-int dmat[20][20],n,i,j,k,count;
-printf("Enter the number of nodes: ");
-scanf("%d",&n);
-printf("\nEnter the cost matrix (99 for no link): ");
-for(i=0;i<n;i++)
-for(j=0;j<n;j++)
-{
-scanf("%d",&dmat[i][j]);
-dmat[i][i]=0;
-rt[i].dist[j]=dmat[i][j];
-rt[i].from[j]=j;
-}
-do
-{
-count=0;
-for(i=0;i<n;i++)
-for(j=0;j<n;j++)
-for(k=0;k<n;k++)
-if(rt[i].dist[j]>dmat[i][k]+rt[k].dist[j])
-{
-rt[i].dist[j]=dmat[i][k]+rt[k].dist[j];
-rt[i].from[j]=k;
-count++;
-}
-}while(count!=0);
-for(i=0;i<n;i++)
-{
-printf("\nState value of router %d is:",i+1);
-for(j=0;j<n;j++)
-printf("\n\tNode %d via %d distance = %d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
-printf("\n");
-}
-return 0;
+    int n, count;
+    printf("Enter the number of nodes:");
+    scanf("%d", &n);
+    int c[n][n];
+    printf("Enter the cost matrix of the nodes:\n  ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", i + 1);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", i + 1);
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &c[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+
+                continue;
+            }
+            else
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    if (c[i][j] > c[k][j] + c[i][k])
+                    {
+                        c[i][j] = c[k][j] + c[i][k];
+                    }
+                }
+            }
+        }
+    }
+    printf("The final routing table is: \n  ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", i + 1);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", i + 1);
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
 }
--- NL/SAW.c
+++ NL/SAW.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 int main()
 {
-int i=1,noframes=10,x,x1=10;
-printf("Number of frames is %d\n",noframes);
-while(noframes>0)
-{
-printf("\nSending frame %d",i);
-srand(x1++);
-x = rand()%10;
-if(x%2 == 0)
-{
-printf("\nWaiting for 1 seconds");
-sleep(1);
-printf("\nMissing Acknowledgement %d\nSending frame %d",i,i);
-}
-printf("\nAcknowledgement received for frame %d\n",i);
-noframes-=1;
-i++;
-}
-printf("\nEnd of stop and wait protocol");
-return(0);
+    int i = 1, noframes = 10, x, x1 = 10;
+    printf("Number of frames is %d\n", noframes);
+    while (noframes > 0)
+    {
+        printf("\nSending frame %d", i);
+        srand(x1++);
+        x = rand() % 10;
+        if (x % 2 == 0)
+        {
+            printf("\nWaiting for 1 seconds");
+            sleep(1);
+            printf("\nMissing Acknowledgement %d\nSending frame %d", i, i);
+        }
+        printf("\nAcknowledgement received for frame %d\n", i);
+        noframes -= 1;
+        i++;
+    }
+    printf("\nEnd of stop and wait protocol");
+    return (0);
 }
