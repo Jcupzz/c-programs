bad07dbacdfe1c63324aef54aa7debe3955b1d6d
Jcupzz <jcupzz255@gmail.com>
2022-09-14 14:53:07 +0530

    Added the smallest program for udp

--- NL/udp_server.c
+++ NL/udp_server.c
@@ -15,56 +15,29 @@ int main(void)
     memset(server_message,'\0', sizeof(server_message));
     memset(client_message,'\0', sizeof(client_message));
 
-
     // Create socket:
-
-    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-    if (socket_desc < 0)
-    {
-        printf(" Error while creating socket \n ");
-        return -1;
-    }
-
+    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // NEW
     printf(" Socket created successfully \n ");
 
     // Set port and IP:
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(5000);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+   
     // Bind to the set port and IP :
-    if (bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
-    {
-        printf(" Couldn't bind to the port \n ");
-        return -1;
-    }
-
+    bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr));
     printf(" Done with binding \n ");
-    // Listen for clients :
-    if (listen(socket_desc, 1) < 0)
-    {
-        printf(" Error while listening \n ");
-        return -1;
-    }
-    printf(" \nListening for incoming connections ..... \n ");
-     if (recvfrom(socket_desc, client_message, sizeof(client_message), 0,(struct sockaddr*)&client_addr,&client_struct_length) < 0)
-    {
-        printf(" Couldn't receive \n ");
-        return -1;
-    }
-    printf(" Client connected at IP : %s and port : %i \n ", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-    printf(" Msg from client : %s \n ", client_message);
     
+    // Listen for clients :
+    printf(" \nListening for incoming connections ..... \n ");
+    recvfrom(socket_desc, client_message, sizeof(client_message), 0,(struct sockaddr*)&client_addr,&client_struct_length);
+    
+    // Print message from client :  
+    printf(" Msg from client : %s \n ", client_message);
+
     // Respond to client :
     strcpy(server_message, " This is the server's message . ");
-    if (sendto(socket_desc, server_message, strlen(server_message), 0,(struct sockaddr*)&client_addr,&client_struct_length) < 0)
-    {
-        printf(" Can't send \n ");
-        return -1;
-    }
-
+    sendto(socket_desc, server_message, strlen(server_message), 0,(struct sockaddr*)&client_addr,&client_struct_length);
 
     // Closing the socket :
     close(socket_desc);
